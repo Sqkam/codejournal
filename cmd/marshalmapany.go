9417fcfb57ef5c2ab7e5086085754f0312e1632e
Sqkam <paopaosqkam@icloud.com>
2024-04-06 23:58:37 +0800

    export MarshalMapAny

--- cmd/marshalmapany.go
+++ cmd/marshalmapany.go
@@ -8,60 +8,61 @@ import (
 
 func main() {
 	kk := `{
-    "a": 1,
-    "b": {
-        "c": 2,
-        "d": {
-            "e": 3,
-            "f": [4, 5, 6]
-        }},
-    "inbounds": [ 
-        {
-            "listen": "0.0.0.0",
-            "port": 443,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "",
-                        "flow": "xtls-rprx-vision" 
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "reality",
-                "realitySettings": {
-                    "show": false, 
-                    "dest": "example.com:443", 
-                    "xver": 0,
-                    "serverNames": [
-                        "example.com",
-                        "www.example.com"
-                    ],
-                    "privateKey": "",
-                    "minClientVer": "", 
-                    "maxClientVer": "", 
-                    "maxTimeDiff": 0, 
-                    "shortIds": [
-                        "", 
-                        "0123456789abcdef" 
-                    ]
-                }
-            }
-        }
-    ]
-}`
+	   "a": 1,
+	   "b": {
+	       "c": 2,
+	       "d": {
+	           "e": 3,
+	           "f": [4, 5, 6]
+	       }},
+	   "inbounds": [
+	       {
+	           "listen": "0.0.0.0",
+	           "port": 443,
+	           "protocol": "vless",
+	           "settings": {
+	               "clients": [
+	                   {
+	                       "id": "",
+	                       "flow": "xtls-rprx-vision"
+	                   }
+	               ],
+	               "decryption": "none"
+	           },
+	           "streamSettings": {
+	               "network": "tcp",
+	               "security": "reality",
+	               "realitySettings": {
+	                   "show": false,
+	                   "dest": "example.com:443",
+	                   "xver": 0,
+	                   "serverNames": [
+	                       "example.com",
+	                       "www.example.com"
+	                   ],
+	                   "privateKey": "",
+	                   "minClientVer": "",
+	                   "maxClientVer": "",
+	                   "maxTimeDiff": 0,
+	                   "shortIds": [
+	                       "",
+	                       "0123456789abcdef"
+	                   ]
+	               }
+	           }
+	       }
+	   ],
+	"matrix":[[1,2,3],[4,5,6]]
+	}`
 	var data interface{}
 	err := json.Unmarshal([]byte(kk), &data)
 	if err != nil {
 		panic(err)
 	}
-	marshalMapAny(data, "")
+	MarshalMapAny(data, "")
 }
 
-func marshalMapAny(data interface{}, prefix string) {
+func MarshalMapAny(data interface{}, prefix string) {
 	switch t := data.(type) {
 	case map[string]interface{}:
 		for key, value := range t {
@@ -72,7 +73,7 @@ func marshalMapAny(data interface{}, prefix string) {
 				newKey = key
 			}
 
-			marshalMapAny(value, newKey)
+			MarshalMapAny(value, newKey)
 
 		}
 	case []interface{}:
@@ -83,7 +84,7 @@ func marshalMapAny(data interface{}, prefix string) {
 			} else {
 				newKey = "[" + fmt.Sprintf("%d", index) + "]"
 			}
-			marshalMapAny(value, newKey)
+			MarshalMapAny(value, newKey)
 		}
 	case string, bool, float64:
 		fmt.Printf("%s=%v\n", prefix, t)
